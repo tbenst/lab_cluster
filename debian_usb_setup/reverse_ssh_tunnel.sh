f49e7c190f342243b8a9c57d05f630137ceb52fd
tbenst <tbenst@gmail.com>
2016-05-05 16:33:15 -0700

    new keys

--- debian_usb_setup/reverse_ssh_tunnel.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-COMMAND="ssh -N -f -R 4242:localhost:2424 tyler@alexon.duckdns.org"
-pgrep -f -x "$COMMAND" > /dev/null 2>&1 || $COMMAND
--- debian_usb_setup/test.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-default=192.168.0.4
-read -p "Enter IP address [$default]: " IP
-IP=${IP:-$default}
-echo "IP is $IP"
